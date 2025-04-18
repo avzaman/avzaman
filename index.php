33516719bbc940126940b070fbe630ed7115ec34
avzaman <vin.zaman@gmail.com>
2025-04-17 20:54:25 -0400

    php test for vercel -Vince

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php include 'pages/header.html'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amir Zaman Portfolio Site</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<?php require 'pages/header.html'; ?>
 
 <body>
     &lt;Website under maintanence, be back soon :)&gt;
 </body>
 
-<?php include 'pages/footer.html'; ?>
+<?php require 'pages/footer.html'; ?>
+</html>
